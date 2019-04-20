87441683f9760a7ffeefefea30dfb037f88e2655
Linas Šlepikas <linasslepikas@gmail.com>
2019-04-20 09:28:54 +0300

    Added company validator.

--- shipment_discount_calculation_module.py
+++ shipment_discount_calculation_module.py
@@ -1,4 +1,4 @@
-PRICING = {
+DATA = {
     "LP": {
         "S": "1.50",
         "M": "4.90",
@@ -13,17 +13,19 @@ PRICING = {
 
 
 def process_input_data(data='input.txt'):
-    input_data = []
+    input_data = {}
     with open(data) as f:
         content = f.readlines()
-    # you may also want to remove whitespace characters like `\n` at the end of each line
     content = [x.strip() for x in content]
 
-    # print(content)
     for item in content:
-        input_data.append(item)
+        data = item.split()
+        if "S" in data:
+            data.append(lowest_price(DATA))
+        print(data)
+        # input_data.append(item)
 
-    print(input_data)
+    return input_data
 
 
 def lowest_price(provider_pricing, package_size='S'):
@@ -32,3 +34,22 @@ def lowest_price(provider_pricing, package_size='S'):
         small_price.append(value[package_size])
     small_price.sort()
     return small_price[:1][0]
+
+
+def add_smallest_price(*args):
+    pass
+
+
+def validate_provider(provider, data):
+    for key, value in data.items():
+        if key == provider:
+            return True
+    return False
+
+
+def validate_date(date):
+    return False
+
+
+def validate_package_size(data):
+    return False
