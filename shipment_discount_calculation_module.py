22ec2a2085cc53d7b8908aef5eb0b7da5d1319db
Linas Slepikas <lisl@danskebank.lt>
2019-04-26 18:57:37 +0300

    Fixed gug for data duplication after remapping info to ordered data.

--- shipment_discount_calculation_module.py
+++ shipment_discount_calculation_module.py
@@ -15,71 +15,92 @@ DATA = {
 }
 
 
-def process_input_data(data='input.txt'):
-    input_data = {}
-    output_data = []
+# 1. validating input format
+def validate_input_format(data):
+    extension = data.split(".")[1]
+    print(extension)
+    if extension != 'txt':
+        print('Input file format not valid, only .txt files add expected.')
+        exit(1)
+    return True
+
+
+# 2. Read input format
+def read_data(data):
     with open(data) as f:
         content = f.readlines()
     content = [x.strip() for x in content]
+    return content
 
-    # counting L discount
-    print(content)
+
+# 3. Order data by date in descending order
+def order_data_asc(data):
     dates = []
-    for item in content:
+    for item in data:
         row = item.split()
         dates.append(row[0])
 
-    sorted_dates = sorted(dates, key=lambda x: datetime.datetime.strptime(x, '%Y-%m-%d'))
-
-    dates_list = []
-
-    for item in sorted_dates:
-        row = item.split()
-        dates_list.append(row)
-    # print(dates_list)
-
-    for item in content:
-        for date in dates_list:
-            if item.split(" ")[0] in date:
-                for a in item.split(" ")[1:]:
-                    date.append(a)
-
-    print(dates_list)
+    # Order dates in ASC order
+    dates_in_asc_order = sorted(dates, key=lambda x: datetime.datetime.strptime(x, '%Y-%m-%d'))
+    output = []
+    # Looping via dates
+    for date in dates_in_asc_order:
+        # Looping via data provider
+        for key in data:
+            if date in key.split():
+                # if that date is in the data provider, we add that data to output
+                output.append(key)
+                # since we found data map we remove this information from data ser to avoid duplication
+                data.remove(key)
+                break
+    return output
 
 
+def output_data(data):
+    output = []
+    for item in data:
+        output.append(' '.join(item))
+    for out in output:
+        print(out)
+    return True
 
+
+
+
+data = read_data('input.txt')
+
+ordered_data = order_data_asc(data)
+
+
+
+
+
+
+
+def process_input_data(content):
+    output_data = []
     l_discount = {}
     for item in content:
-        row = item.split()
-        if 'L' in row and 'LP'in row:
-            month = datetime.datetime.strptime(row[0], '%Y-%m-%d').month
+        if 'L' in item and 'LP'in item:
+            month = datetime.datetime.strptime(item[0], '%Y-%m-%d').month
             try:
-                l_discount[month].append(row[0])
+                l_discount[month].append(item[0])
             except KeyError:
-                l_discount[month] = [row[0]]
+                l_discount[month] = [item[0]]
                 l_discount['discount_{}'.format(month)] = False
 
             if len(l_discount[month]) == 3 and not l_discount['discount_{}'.format(month)]:
-                row.append('0.00')
+                item.append('0.00')
                 l_discount['discount_{}'.format(month)] = True
-        # print(row)
-
-    # print(l_discount)
-
-    # datetime_object = datetime.datetime.strptime('2012-09-22', '%Y-%m-%d')
-    # print(datetime_object.month)
 
     for item in content:
-        data = item.split()
-        # print(data)
-        if len(data) != 3:
-            data.append('Ignored')
-        if "S" in data:
-            data.append(lowest_price(DATA))
-        output_data.append(data)
-        # input_data.append(item)
-    # for output in output_data:
-    #     print(' '.join(output))
+        if len(item) < 3:
+            item.append('Ignored')
+        if "S" in item:
+            item.append(lowest_price(DATA))
+        output_data.append(item)
+
+    return output_data
 
 
 def lowest_price(provider_pricing, package_size='S'):
@@ -88,6 +109,11 @@ def lowest_price(provider_pricing, package_size='S'):
         small_price.append(value[package_size])
     small_price.sort()
     return small_price[:1][0]
+
+
+# processed_data = process_input_data(ordered_data)
+
+output_data(ordered_data)
 
 
 def add_smallest_price(*args):
@@ -118,4 +144,4 @@ class ShipmentValidator:
 
 
 # print(ShipmentValidator.validate_provider('MR', DATA))
-process_input_data()
+# process_input_data()
